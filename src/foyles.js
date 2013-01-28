19234ea7af0da6f56d37f5e422c812a6198390fb
Edmund von der Burg <evdb@mysociety.org>
2013-01-28 10:50:11 -0300

    Provide applicable countries as an array, to make it easier to store in database.

--- src/foyles.js
+++ src/foyles.js
@@ -45,7 +45,7 @@ Scraper.prototype.jqueryExtract = function ($) {
       }
       
       var price = {
-        destination:     'GB',
+        contries:       ['GB'],
         condition:       'new',
         currency:        'GBP',
         shippingComment: 'Free delivery in the UK for orders over £10',
