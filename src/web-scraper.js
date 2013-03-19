faf7fc4e2c979670bc27a6f2eeec8e16a87815e1
Edmund von der Burg <evdb@mysociety.org>
2013-03-19 07:31:50 +0000

    Extract the common scraper parts into a new module

--- src/web-scraper.js
+++ src/web-scraper.js
@@ -5,36 +5,17 @@ var util    = require('util'),
     jsdom   = require('jsdom'),
     fs      = require('fs'),
     request = require('request'),
-    ean     = require('ean');
+    ean     = require('ean'),
+    GeneralScraper = require('./general-scraper');
 
 var jquerySource  = fs.readFileSync(__dirname + '/../lib/jquery.js').toString();
-
-var defaultTTL = 86400;
 
 var scraper = function () {
   
 };
 
-scraper.prototype.init = function (options) {
-  _.extend(this, options);
-  
-  var isbn = this.isbn;
+scraper.prototype = new GeneralScraper();
 
-  // Check that we have the values we need
-  if (!isbn) {
-    throw new Error('Need an isbn');
-  }
-
-  if (!ean.isValid(isbn)) {
-    throw new Error('Not a valid ISBN13 "' + this.isbn + '"');
-  }
-
-  this.isbn = isbn;
-
-  this.results = {
-    isbn: isbn
-  };
-};
 
 scraper.prototype.get = function (url, cb) {
   var results = this.results;
@@ -67,6 +48,7 @@ scraper.prototype.get = function (url, cb) {
   );
 };
 
+
 scraper.prototype.scrape = function (cb) {
   var self    = this;
   var results = this.results;
@@ -97,78 +79,5 @@ scraper.prototype.scrape = function (cb) {
   
 };
 
-
-scraper.prototype.cleanup = function (results) {
-  var self = this;
-
-  if (!results.found) {
-
-    results.prices = [];
-
-    _.each(self.countries, function (country) {
-      _.each(self.currencies, function (currency) {
-        results.prices.push({
-          country: country,
-          currency: currency,
-          availability: false,
-          availabilityComment: 'Not found',
-        });
-      });
-    });
-  }
-
-
-  _.each(results, function (val, key) {
-    if (_.isString(val)) {
-      val = val.replace(/\s+/, ' ');
-      results[key] = val.trim();
-    }
-  });
-  
-  _.each(results.prices, function (price) {
-    
-    // Initial defaults
-    _.defaults(price, {
-      amount:   false,
-      shipping: false,
-      total:    false,
-      ttl:      self.defaultTTL || defaultTTL,
-      url:      results.url,
-    });
-
-    // Defaults that depend on other values
-    _.defaults(price, {
-      validUntil: Math.floor(results._startTime / 1000 + price.ttl)
-    });
-
-    if (_.isNumber(price.amount) && _.isNumber(price.shipping)) {
-      price.total = price.amount + price.shipping;
-    }
-
-    price.availability = self.parseAvailability(price);
-
-    // Set regardless to ensure that they are correct
-    price.isbn   = self.isbn;
-    price.vendor = self.vendorCode;
-
-  });
-  
-  return results;
-};
-
-scraper.prototype.parseAvailability = function (price) {
-
-  var tester = function (regex) {
-    return regex.test(price.availabilityComment);
-  };
-
-  var yesTests = this.availabilityTests.yes;
-  var noTests  = this.availabilityTests.no;
-  
-  if (_.any(yesTests, tester)) { return true;  }
-  if (_.any(noTests,  tester)) { return false; }
-  
-  return null;
-};
 
 module.exports = scraper;
