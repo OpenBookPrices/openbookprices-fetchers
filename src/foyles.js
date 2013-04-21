2e1606476a0795c2d1f846d37daf0c0027b6fdc3
Edmund von der Burg <evdb@mysociety.org>
2013-04-21 17:40:39 +0100

    Add in country data, refactor Foyles scraper for UK

--- src/foyles.js
+++ src/foyles.js
@@ -1,7 +1,9 @@
 'use strict';
 
 var WebScraper = require('./web-scraper'),
+    countries  = require('country-data').countries,
     _          = require('underscore');
+
 
 // note that we can get the worldwide shipping prices from
 // http://www.foyles.co.uk/help-delivery
@@ -17,6 +19,29 @@ Scraper.prototype.vendorCode = 'foyles';
 Scraper.prototype.countries  = ['GB'];
 Scraper.prototype.currencies = ['GBP'];
 
+// For shipping rates http://www.foyles.co.uk/help-delivery
+var regions = {}
+
+regions.uk = ['GB'];
+
+regions.europe = [
+  // Austria, Belgium, Denmark, France, Germany, Greece, Iceland,
+  // Irish Republic, Italy, Luxembourg, Netherlands, Portugal, Spain,
+  // Sweden and Switzerland
+  'AT', 'BE', 'DK', 'FR', 'DE', 'GR', 'IS', 'IE', 'IT', 'LU', 'NL', 'PT',
+  'ES', 'SE', 'CH'
+];
+
+regions.northAmerica = ['US', 'CA'];
+
+regions.restOfWorld = _.difference(
+  _.pluck(countries.all, 'alpha2'),
+  regions.uk, regions.europe, regions.northAmerica
+);
+
+
+console.log( regions );
+
 
 Scraper.prototype.isbnURLTemplate = 'http://www.foyles.co.uk/Public/Shop/Search.aspx?sortBy=1&searchType=3&advance=true&isbn={isbn}';
 
@@ -24,7 +49,7 @@ Scraper.prototype.isbnURLTemplate = 'http://www.foyles.co.uk/Public/Shop/Search.
 Scraper.prototype.jqueryExtract = function ($) {
 
   var results = {};
-  
+
   results.found = ! /No search results/.test($('h2.MainTitle').text());
 
   results.title  = $('div.BookTitle').find('span[itemprop=name]').text();
@@ -34,7 +59,7 @@ Scraper.prototype.jqueryExtract = function ($) {
   );
 
   var prices = results.prices = [];
-  
+
   $('div.PurchaseTable')
     .find('tr.DarkGrey')
     .first()
@@ -43,26 +68,22 @@ Scraper.prototype.jqueryExtract = function ($) {
       if (! row.attr('class')) {
         return;
       }
-      
-      var price = {
-        countries:       ['GB'],
+
+      var basePrice = {
         condition:       'new',
         currency:        'GBP',
-        shippingComment: 'Free delivery in the UK for orders over £10',
-        shipping:        2.5,
       };
-      
-      price.amount = parseFloat(row.find('.OnlinePrice').text().replace(/[\D\.]/, '')) || null;
 
-      price.availabilityComment = row.find('.Availtext').text().trim();
+      var amount = basePrice.amount = parseFloat(row.find('.OnlinePrice').text().replace(/[\D\.]/, '')) || null;
+      basePrice.availabilityComment = row.find('.Availtext').text().trim();
 
-      if (price.amount >= 10) {
-        price.shipping = 0;
-      }
-
-      prices.push(price);
+      prices.push(_.extend(basePrice, {
+        countries: regions.uk,
+        shipping: amount < 10 ? '2.50' : 0,
+        shippingComment: 'Free delivery in the UK for orders over £10',
+      }));
     });
-  
+
   return results;
 };
 
