8890afed8990b3f5a940288cba87bc0881752df4
Edmund von der Burg <evdb@mysociety.org>
2013-08-26 13:47:15 +0100

    Add config and create initial entries

---
+++ config/default.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "amazon_uk": {
+    // disabled by default as scraper requires auth tokens
+    disabled:  true,
+    awsId:     null,
+    awsSecret: null,
+    assocId:   null,
+  },
+};
+
