2454e270def12c33e417e15fff8cb12ea454236d
Edmund von der Burg <evdb@mysociety.org>
2013-08-27 08:22:42 +0100

    Put the default amazon_uk config into the scraper

--- src/amazon_uk.js
+++ src/amazon_uk.js
@@ -6,6 +6,14 @@ var GeneralScraper     = require("./general-scraper"),
     config             = require("config"),
     shipping           = require("./amazon_uk_shipping");
 
+config.setModuleDefaults("amazon_uk", {
+  // disabled by default as scraper requires auth tokens
+  disabled:  true,
+  awsId:     null,
+  awsSecret: null,
+  assocId:   null,
+});
+
 var Scraper = module.exports = function (options) {
   this.init(options);
 };
